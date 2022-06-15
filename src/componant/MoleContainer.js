dce0a8472b1ea6bf4358032cacdf7ed319e2649a
AugieDog <67617205+Augiedog@users.noreply.github.com>
2022-06-15 13:57:33 -0500

    Working from work

--- src/componant/MoleContainer.js
+++ src/componant/MoleContainer.js
@@ -2,24 +2,20 @@ import Mole from './Mole'
 import NoMole from './EmptySlot'
 
 function MoleContainer(props) {
+    let [theMole, setTheMole] = useState(false)
 
     // A handleClick function that will increment the score by 1 and immediately set the clicked-on mole to no longer display (we will pass this down only to Mole components)
     const handleClick = (e) => {
+        prop.setScore(props.score ++)
         display(false)
     }
 
     // A ternary that will determine which child to render
-    const display = () => {
-        return (
-            <>
-                <Mole /><NoMole />
-                
-            </>
-        )
+    const display = theMole ? <Mole setScore={props.setScore} toggle={setTheMole} handleClick={handleClick} /> : <NoMole toggle={setTheMole} />
     }
 
     return (
-        <div className='mole-container'>
+        <div style={{'display': 'inline-block', 'width': '25vw'}}>
             {display()}
         </div>
     )
