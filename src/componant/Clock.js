c3ef8c5092e034a437058ea436a44654b0817600
AugieDog <67617205+Augiedog@users.noreply.github.com>
2023-07-25 14:33:51 -0500

    on the road.js

--- src/componant/Clock.js
+++ src/componant/Clock.js
@@ -16,3 +16,34 @@ function Clock(props) {
 }
 
 export default Clock
+
+const Timer = () => {
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  const deadline = "December, 31, 2022";
+
+  const getTime = () => {
+    const time = Date.parse(deadline) - Date.now();
+
+    setDays(Math.floor(time / (1000 * 60 * 60 * 24)));
+    setHours(Math.floor((time / (1000 * 60 * 60)) % 24));
+    setMinutes(Math.floor((time / 1000 / 60) % 60));
+    setSeconds(Math.floor((time / 1000) % 60));
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => getTime(deadline), 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="timer">
+    </div>
+  );
+};
+
+export default Timer
