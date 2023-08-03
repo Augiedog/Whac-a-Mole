f77d8ee4432fb12b262c91113456bf16621fd584
AugieDog <67617205+Augiedog@users.noreply.github.com>
2023-08-03 17:59:47 -0500

    Update HighScore.js

--- frontend/src/componant/HighScore.js
+++ frontend/src/componant/HighScore.js
@@ -8,6 +8,22 @@ function HighScore(props) {
 
     {/* A function that will show scores of last 30 days
     then displays a table with the scores from highest to lowest */}
+const TableFromObject = ({ data }) => {
+  const tableRows = Object.entries(data).map(([key, value]) => (
+    <tr key={key}>
+      <td>{key}</td>
+      <td>{value}</td>
+    </tr>
+  ));
+
+  return (
+    <table>
+      <tbody>
+        {tableRows}
+      </tbody>
+    </table>
+  );
+};
         {/* <p>{props.topScore.name}</p>
         <p>{props.topScore.score}</p> */}
         {/* <h4>Error: {props.error}</h4> */}
