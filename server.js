fe50d0f83517c4b7eb94f3e52b9cc5808daefaba
Augie Bren <Augie>
2023-09-05 22:09:11 -0500

    rebuild

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.use('/highscore', router)
 
 // db connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('DB connected'))
     .catch(err => console.error(err));
 
 // Listen for Connection
