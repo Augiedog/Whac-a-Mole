efa67cd4effc08bd4a2d0e950d9814de05363e4e
Augie Bren <Augie>
2023-09-05 21:58:59 -0500

    build timeout?

--- server.js
+++ server.js
@@ -25,7 +25,6 @@ app.use('/highscore', router)
 
 // db connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('DB connected'))
     .catch(err => console.error(err));
 
 // Listen for Connection
