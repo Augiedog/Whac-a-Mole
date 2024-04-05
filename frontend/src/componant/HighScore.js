a1899f9047d3593f1358393da50e8f9e9eba26f5
Augie Bren <Augie>
2024-04-04 20:49:38 -0500

    longer box

--- frontend/src/componant/HighScore.js
+++ frontend/src/componant/HighScore.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react"
 import axios from 'axios'
 import NewHigh from "./NewHigh"
+import './HighScore.css'
 
 function HighScore(props) {
     let [topScore, setTopScore] = useState([])
